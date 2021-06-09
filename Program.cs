e2dc5dbb61708ce3f222d9a613fe6a83d62db507
DevelopeJr <edemir.ferreira.jr@gmail.com>
2021-06-08 22:41:07 -0300

    [ Etapa-07-HandsOn ]

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using Dominio;
+using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var empregado1 = new Analista("Julio", 15000);
+            Console.WriteLine($"Nome: {empregado1.Nome}");
+            Console.WriteLine($"Salário: {empregado1.Salario:N2}");
+            Console.WriteLine($"Salário bonificado: {empregado1.CalcularSalarioBonificado():N2}");
+
+            var empregado2 = new Vendedor("Julio Analista", 15000);
+            Console.WriteLine($"Nome: {empregado2.Nome}");
+            Console.WriteLine($"Salário: {empregado2.Salario:N2}");
+            Console.WriteLine($"Salário bonificado: {empregado2.CalcularSalarioBonificado():N2}");
+
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine($"{empregado1.ToString()}");
+            Console.WriteLine($"{empregado2.ToString()}");
+
+
+        }
+    }
+}
